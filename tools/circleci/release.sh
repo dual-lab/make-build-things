616be88181398a43ec8dd8ba9f0daf0a0a0b7aa8
dmike16 <cipmiky@gmail.com>
2019-04-27 01:00:54 +0200

    build: [2/5] create release script

--- tools/circleci/release.sh
+++ tools/circleci/release.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-set -x -u -e -o pipefail
+set -x -u -o pipefail
 
 source "tools/circleci/decrypt_token.sh"
 source "tools/version.sh"
+
+readonly REPO_ENDPOINT="https://api.github.com/repos/dual-lab/make-build-thing"
+readonly AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 ##
 # name   : release.sh
 # author : dmike
@@ -11,33 +14,53 @@ source "tools/version.sh"
 #        creating a new rel on github and uploading the artifact
 ##
 function main() {
-    local current_branch=$(git rev-parse --abbrev-ref  HEAD)
+    
+    assert_master_branch
+    #exit_on_error $? "Release only from master branch"
 
-    printf "##\n"
-    printf "#  Released script \n"
-    printf "#  Version: ${MAJOR}.${MINOR}.${PATCH}\n"
-    printf "#  Branch: ${current_branch}\n"
-    printf "#  Creating shar archive.....\n"
+    local tag="v${MAJOR}.${MINOR}.${PATCH}"
 
-    if [ "$current_branch" != "master" ]; then
-        printf 'Error: relese only from master branch\n'
-        exit 1;
-    fi
+    assert_no_tag_Version $tag
+    exit_on_error $? "Release with tag ${tag}, already exists"
 
-    local artifactid = "${bundle_name}-${MAJOR}.${MINOR}.${PATCH}" 
+    local bundle_name="mkbts"
+    local artifactid="${bundle_name}-${tag}" 
     generate_bundle ${artifactid}
-
-    printf "##\n"
 }
 
 function generate_bundle(){
-    shar \
-    --input-file-list=tools/released_files.txt \
-    --archive-name=$1 \
-    --submitter=dmike16@somewhere \
-    --net-headers \
-    -o $1
+    tar -zcvf "$1.tar.gz" \
+    KBUILD Makefile LICENSE CHANGELOG.md README.md \
+    scripts/
+
     return $?
+}
+
+function assert_master_branch(){
+    local current_branch="$(git rev-parse --abbrev-ref  HEAD)"
+    local expected_branch="master"
+    
+    if [[ "$current_branch" == "$expected_branch" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function assert_no_tag_Version(){
+    local v_tag=$1
+    local tag_url="${REPO_ENDPOINT}/releases/tags/${v_tag}"
+    local status=$(curl --silent -H "${AUTH_HEADER}" --head ${tag_url} | awk '/Status:/ {print $2}')
+
+    ((${status} == 404)) 
+    return $?
+}
+
+function exit_on_error(){
+    if [[ $1 -ne 0 ]]; then
+        printf "+ Error: $2\n"
+        exit 1
+    fi
 }
 
 main
