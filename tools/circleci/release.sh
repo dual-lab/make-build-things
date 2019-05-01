9f9718bcbef5fbb87814a17b39e1c44189045596
dmike16 <cipmiky@gmail.com>
2019-05-01 18:01:25 +0200

    build: [3/5] create release script

--- tools/circleci/release.sh
+++ tools/circleci/release.sh
@@ -5,9 +5,10 @@ set -e -x -u -o pipefail
 source "tools/circleci/decrypt_token.sh"
 source "tools/version.sh"
 
-readonly REPO_ENDPOINT="https://api.github.com/repos/dual-lab/make-build-thing"
+readonly REPO_ENDPOINT="https://api.github.com/repos/dual-lab/make-build-things"
 readonly AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
-readonly CONTENT_TYPE="Content-Type:Application/json"
+readonly JSON_TYPE="Content-Type:Application/json"
+readonly ZIP_TYPE="Content-Type:Application/zip"
 ##
 # name   : release.sh
 # author : dmike
@@ -16,8 +17,9 @@ readonly CONTENT_TYPE="Content-Type:Application/json"
 ##
 function main() {
     
-    assert_master_branch
+    #assert_master_branch
 
+    local version="${MAJOR}.${MINOR}.${PATCH}"
     local tag="v${MAJOR}.${MINOR}.${PATCH}"
 
     assert_no_tag_version ${tag}
@@ -26,8 +28,7 @@ function main() {
     local artifactid="${bundle_name}-${tag}"
 
     generate_bundle ${artifactid}
-    ## TODO function that extract portion of changelog
-    generate_release ${tag} ${artifactid}
+    generate_release ${tag} ${version} ${artifactid}
 }
 
 function generate_bundle(){
@@ -64,10 +65,37 @@ function assert_no_tag_version(){
 }
 
 function generate_release(){
-    local rel_url="${REPO_ENDPOINT}/release"
-    local rel_payload="{\"tag_name\":\"$1\", \"name\":\"$1\",\"draft\":true,\"body\":$3 }"
-    local awk_program='BEGIN{FS="\":"} /current_user_url/ {print substr($NF,1,length($NF)-1)}'
-    local upload_url=$(curl --silent -H "${AUTH_HEADER}" -H "${CONTENT_TYPE}" -d ${rel_payload} ${rel_url} | awk "${awk_program}")
+    local rel_url="${REPO_ENDPOINT}/releases"
+    local awk_extract_field='BEGIN{FS="\":"} /upload_url/ {print substr($NF,1,length($NF)-1)}'
+    local bundle="$3.tar.gz"
+    local awk_extract_changelog="BEGIN{RS=\"---\n\n\";FS=\"\n\"} {if(index(\$1,\"## [$2]\") > 0){for(i=2; i<=NF; i++){ printf \"%s\\\\n\",\$i }}}"
+    local changelog_extract=$(cat CHANGELOG.md | awk "${awk_extract_changelog}")
+
+    local rel_payload="{\"tag_name\":\"$1\", \"name\":\"$3\",\"draft\":true,\"body\": \""${changelog_extract}"\" }"
+    local upload_url=$(curl --silent -H "${AUTH_HEADER}" -H "${JSON_TYPE}" -X POST -d "${rel_payload}" ${rel_url} | awk "${awk_extract_field}")
+    
+    if [[ -z ${upload_url} ]]; then
+        printf "Upload url empty. File non upload correctly\n"
+        return 1
+    else
+        upload_url=${upload_url/\{?name,label\}/?name=${bundle}}
+        upload_url=${upload_url//\"/}
+        
+        upload_artifact ${upload_url} ${bundle}
+    fi
+}
+
+function upload_artifact(){
+    local url=$1
+    local artifactid=$2
+
+    local upload_status=$(curl -i --silent -H "${AUTH_HEADER}" -H "${ZIP_TYPE}" -X POST --data-binary @${artifactid} ${url} | awk '/201 Created/ {print $2}')
+
+    if [[ ${upload_status} -eq 201 ]]; then
+        return 0
+    else 
+        return 1
+    fi
 }
 
 main
