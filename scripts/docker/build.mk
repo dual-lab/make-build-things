f5d2a685e01a9d04c818828f6f4701cd2410e415
dmike16 <cipmiky@gmail.com>
2019-06-21 23:30:07 +0200

    feat(docker): add docker plugin
    - build target that dependes on pre_build
    - clean target
    - clobber target

---
+++ scripts/docker/build.mk
@@ -0,0 +1,43 @@
+# ============================================================================ #
+#
+#  Author       : dmike
+#  Date         : 16 June 2019
+#  Description  : 
+#
+# ============================================================================ #
+
+PHONY := __build
+__build: __init_build
+
+# ============================================================================ #
+# Include utilities function
+# ============================================================================ #
+include $(root)/scripts/include.mk
+
+quiet_cmd_init_build = $(call LOG,$(INFO),== Docker build in $(marker))
+color_cmd_init_build = $(c_cyang)$(quiet_cmd_init_build)
+cmd_init_build =
+
+PHONY += __init_build
+__init_build:
+	$(call cmd,init_build)
+
+quiet_cmd_end_build = $(call LOG,$(INFO),== Docker build out $(marker))
+color_cmd_end_build = $(c_cyang)$(quiet_cmd_end_build)
+cmd_end_build =
+
+PHONY += docker_build_target
+__build: docker_build_target
+
+docker_target := $(addprefix $(target_root)/,$(project_name))
+docker_build_target: $(docker_target)
+	$(call cmd,end_build)
+
+# ============================================================================ #
+# Include file containing the docker_pre_build rule
+# ============================================================================ #
+include $(src_main_root)/$(marker)/Makefile
+
+$(docker_target): $(docker_pre_build)
+
+.PHONY: $(PHONY)
