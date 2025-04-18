c9df587d8fb8522554cbd7ecd78e1daf0e8b9947
dmike16 <cipmiky@gmail.com>
2025-04-18 23:24:12 +0200

    feat: add variable to specify pack to use

--- scripts/packages/_packages_walk.mk
+++ scripts/packages/_packages_walk.mk
@@ -19,19 +19,20 @@ __internal_walking: packages_walking
 # ============================================================================ #
 include $(inp_dir)/Makefile
 
+pkgs ?= $(deps)
 # ============================================================================ #
 # Include utilities function
 # ============================================================================ #
 include $(root)/scripts/include.mk
 
 PHONY += packages_walking
-packages_walking: $(deps)
+packages_walking:  $(pkgs)
 
 quiet_cmd_action_pack = $(call LOG,$(INFO),== Walk package $@)
 color_cmd_action_pack = $(c_yellow)$(quiet_cmd_action_pack)
 cmd_action_pack = $(MAKE) -C $(addprefix $(inp_dir)/,$@) -f $(addprefix $(inp_dir)/,$@/Makefile) $(MAKECMDGOALS)
 
-PHONY += $(deps)
-$(deps):
+PHONY += $(pkgs)
+$(pkgs):
 	$(call cmd,action_pack)
 
